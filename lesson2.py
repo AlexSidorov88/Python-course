c82d928912a9ea22fac29255163a32ad95c36af2
Alex <105877232+AlexSidorov88@users.noreply.github.com>
2023-02-03 11:30:09 +0100

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,10 @@
+print("result:", 5, 7, ".", sep="--", end="") #separate #end of line
+print("Second \" line") #special symbol \"' ;  \n new line ; \t tab 
+print("result:", min(5, 7, 10, -3, 1, 0)) #minimum
+print("result:", max(5, 7, 10, -3, 1, 0)) #maximum
+print("result:", abs(-3)) #module
+print("result:", round(5 / 3)) #rounding
+print("result:", pow(5, 3)) # **
+
+input("Enter your age: ")
+
---
+++ lesson3.py
@@ -0,0 +1,16 @@
+var = 5 #integer
+num = 4.7 #float
+str = "qwerty" #string
+print(var, num, str)  #type of data
+x = '5'
+print(var + int(x)) # str(x) 
+#---------------------------------------------------------------
+
+num1 = int(input("Enter number:")) #calc
+
+num2 = int(input("Enter number:"))
+
+print("Result:", num1 + num2)
+print("Result:", num1 - num2)
+print("Result:", num1 * num2)
+print("Result:", num1 / num2)
