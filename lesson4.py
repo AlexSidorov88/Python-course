60ef3f962f6c96531a4534d2d868bd02b2ab5e05
Alex <105877232+AlexSidorov88@users.noreply.github.com>
2023-02-04 13:44:42 +0100

    Add files via upload

---
+++ lesson4.py
@@ -0,0 +1,13 @@
+#   x = True #booleang  
+#   if not x:
+#     print("You are right")
+# else:
+#   print("nope")
+
+data = input()
+number = 5 if data == "Five" else 0 
+# if data == "Five":
+#     number = 5
+# else:
+#     number = 0 
+print(number)
