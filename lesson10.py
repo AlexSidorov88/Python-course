3411f3db3ccc1472473165c41e6d75241676f87f
Alex <105877232+AlexSidorov88@users.noreply.github.com>
2023-02-08 14:53:22 +0100

    Add files via upload

---
+++ lesson10.py
@@ -0,0 +1,17 @@
+#data = set('Lorem ipsum dolor sit amet, consectetur adipiscing elit')
+#print(data)
+
+data2 = {2, 34, 5, 6}
+
+data2.add(25)
+data2.pop()
+data2.clear()
+
+print(data2)
+
+nums = [2, 5, 7, 7, 7, 9, 2]
+
+new_nums = set(nums)   
+print(new_nums)
+
+#set/frozenset
